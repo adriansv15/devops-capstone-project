e81c4d732c93addd81d760fe4e3f9840c9d9f6e6
santosh9 <santosh9@illinois.edu>
2024-08-20 13:57:22 -0400

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -63,15 +63,18 @@ def create_accounts():
 
 # ... place you code here to LIST accounts ...
 
+
 @app.route('/accounts', methods=['GET'])
 def list_accounts():
     accounts = Account.all()
-    account_list =  [account.serialize() for account in accounts]
+    account_list = [account.serialize() for account in accounts]
     return jsonify(account_list), status.HTTP_200_OK
 
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
+
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -94,7 +97,7 @@ def get_accounts(account_id):
 # ... place you code here to UPDATE an account ...
 
 @app.route('/accounts/<int:account_id>', methods=['PUT'])
-def update_account(account_id):
+def update_account(account_id ):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found." )
@@ -104,6 +107,7 @@ def update_account(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+
 
 # ... place you code here to DELETE an account ...
 
